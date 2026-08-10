c131c6b0b8e861f9532fe1469490140111a53c7b
S1ntoz (Стоцкий Никита) <LenovoNikita2017@yandex.ru>
2026-08-10 11:52:23 +0700

    feat: Переопределить шаблон компонента bitrix:catalog.smart.filter для отображение фильтров в каталоге

---
+++ local/templates/znak_v2/components/bitrix/catalog.smart.filter/.default/result_modifier.php
@@ -0,0 +1,77 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+if (isset($arParams["TEMPLATE_THEME"]) && !empty($arParams["TEMPLATE_THEME"]))
+{
+	$arAvailableThemes = array();
+	$dir = trim(preg_replace("'[\\\\/]+'", "/", dirname(__FILE__)."/themes/"));
+	if (is_dir($dir) && $directory = opendir($dir))
+	{
+		while (($file = readdir($directory)) !== false)
+		{
+			if ($file != "." && $file != ".." && is_dir($dir.$file))
+				$arAvailableThemes[] = $file;
+		}
+		closedir($directory);
+	}
+
+	if ($arParams["TEMPLATE_THEME"] == "site")
+	{
+		$solution = COption::GetOptionString("main", "wizard_solution", "", SITE_ID);
+		if ($solution == "eshop")
+		{
+			$theme = COption::GetOptionString("main", "wizard_eshop_adapt_theme_id", "blue", SITE_ID);
+			$arParams["TEMPLATE_THEME"] = (in_array($theme, $arAvailableThemes)) ? $theme : "blue";
+		}
+	}
+	else
+	{
+		$arParams["TEMPLATE_THEME"] = (in_array($arParams["TEMPLATE_THEME"], $arAvailableThemes)) ? $arParams["TEMPLATE_THEME"] : "blue";
+	}
+}
+else
+{
+	$arParams["TEMPLATE_THEME"] = "blue";
+}
+
+$arParams["FILTER_VIEW_MODE"] = (isset($arParams["FILTER_VIEW_MODE"]) && toUpper($arParams["FILTER_VIEW_MODE"]) == "HORIZONTAL") ? "HORIZONTAL" : "VERTICAL";
+$arParams["POPUP_POSITION"] = (isset($arParams["POPUP_POSITION"]) && in_array($arParams["POPUP_POSITION"], array("left", "right"))) ? $arParams["POPUP_POSITION"] : "left";
+
+$arFilteredItems = [];
+
+foreach ($arResult["ITEMS"] as $key => $arItem) {
+    if (empty($arItem["VALUES"])) {
+        continue;
+    }
+
+    if (isset($arItem["PRICE"])) {
+        if ($arItem["VALUES"]["MAX"]["VALUE"] - $arItem["VALUES"]["MIN"]["VALUE"] <= 0) {
+            continue;
+        }
+    }
+
+    $hasActiveValues = false;
+    foreach ($arItem["VALUES"] as $value) {
+        if (!$value["DISABLED"]) {
+            $hasActiveValues = true;
+            break;
+        }
+    }
+    if (!$hasActiveValues) {
+        continue;
+    }
+
+    $arFilteredItems[$key] = $arItem;
+}
+
+$priceItems = [];
+$otherItems = [];
+
+foreach ($arFilteredItems as $key => $item) {
+    if (isset($item["PRICE"])) {
+        $priceItems[$key] = $item;
+    } else {
+        $otherItems[$key] = $item;
+    }
+}
+
+$arResult["ITEMS"] = $priceItems + $otherItems;
