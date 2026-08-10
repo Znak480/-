f5b920d9b9e08df5be71238649a63ec3e59b37c1
S1ntoz (Стоцкий Никита) <LenovoNikita2017@yandex.ru>
2026-08-10 11:31:46 +0700

    feat:Добавить компонент znak:catalog.product для отображения списка товаров

---
+++ local/components/znak/catalog.products/.description.php
@@ -0,0 +1,13 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    "NAME" => "Список товаров",
+    "DESCRIPTION"=>"Универсальный вывод товара с фильтрацией",
+    "PATH"=>[
+       "ID"=>"znak",
+       "NAME"=>"Компоненты компании ООО \"ЗНАК\""
+    ],
+];
+
+?>
