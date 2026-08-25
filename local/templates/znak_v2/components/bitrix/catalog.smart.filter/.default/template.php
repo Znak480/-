2e6def3d56a934c12c45ba102a9e53d59ad3c045
S1ntoz (Стоцкий Никита) <LenovoNikita2017@yandex.ru>
2026-08-25 16:51:43 +0700

    fix: Удалить BX.Iblock.SmartFilter из catalog.smart.filter
    - Исправить конфликт с новой версией фильтра
    - Убрать дублирующую инициализацию

--- local/templates/znak_v2/components/bitrix/catalog.smart.filter/.default/template.php
+++ local/templates/znak_v2/components/bitrix/catalog.smart.filter/.default/template.php
@@ -70,31 +70,6 @@ $this->setFrameMode(true);
 									>
 								</div>
 							</div>
-							<?
-							$arJsParams = array(
-								"leftSlider" => 'left_slider_'.$key,
-								"rightSlider" => 'right_slider_'.$key,
-								"tracker" => "drag_tracker_".$key,
-								"trackerWrap" => "drag_track_".$key,
-								"minInputId" => $arItem["VALUES"]["MIN"]["CONTROL_ID"],
-								"maxInputId" => $arItem["VALUES"]["MAX"]["CONTROL_ID"],
-								"minPrice" => $arItem["VALUES"]["MIN"]["VALUE"],
-								"maxPrice" => $arItem["VALUES"]["MAX"]["VALUE"],
-								"curMinPrice" => $arItem["VALUES"]["MIN"]["HTML_VALUE"],
-								"curMaxPrice" => $arItem["VALUES"]["MAX"]["HTML_VALUE"],
-								"fltMinPrice" => intval($arItem["VALUES"]["MIN"]["FILTERED_VALUE"]) ? $arItem["VALUES"]["MIN"]["FILTERED_VALUE"] : $arItem["VALUES"]["MIN"]["VALUE"] ,
-								"fltMaxPrice" => intval($arItem["VALUES"]["MAX"]["FILTERED_VALUE"]) ? $arItem["VALUES"]["MAX"]["FILTERED_VALUE"] : $arItem["VALUES"]["MAX"]["VALUE"],
-								"precision" => 2,
-								"colorUnavailableActive" => 'colorUnavailableActive_'.$key,
-								"colorAvailableActive" => 'colorAvailableActive_'.$key,
-								"colorAvailableInactive" => 'colorAvailableInactive_'.$key,
-							);
-							?>
-							<script type="text/javascript">
-								BX.ready(function(){
-									window['trackBar<?=$key?>'] = new BX.Iblock.SmartFilter(<?=CUtil::PhpToJSObject($arJsParams)?>);
-								});
-							</script>
 						<?else:?>
 							<div class="filter-group-body">
 								<div class="filter-checkbox-list">
