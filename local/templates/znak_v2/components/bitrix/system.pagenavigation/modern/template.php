200d03b542f8fa31f2c5d0574c36f3d988ff764e
S1ntoz (Стоцкий Никита) <LenovoNikita2017@yandex.ru>
2026-08-25 16:26:00 +0700

    feat: Добавить ajax обработку кнопки "Показать еще" с сохранением постраничной пагинации

--- local/templates/znak_v2/components/bitrix/system.pagenavigation/modern/template.php
+++ local/templates/znak_v2/components/bitrix/system.pagenavigation/modern/template.php
@@ -19,266 +19,245 @@ $strNavQueryString = ($arResult["NavQueryString"] != "" ? $arResult["NavQueryStr
 $strNavQueryStringFull = ($arResult["NavQueryString"] != "" ? "?".$arResult["NavQueryString"] : "");
 ?>
 
-<div class="catalog-pagination-wrapper" id="<?= $containerId ?>" <?= $isAjax ? 'data-ajax="Y"' : '' ?>>
-	
-	<? if ( $arResult["NavRecordCount"] !== $arResult["NavLastRecordShow"] ): ?>
-		<!-- Кнопка "Показать еще" -->
-		<button class="btn btn-outline btn-sm btn-load-more" 
-				data-page="2" 
-				data-total="<?= $arResult["NavPageCount"] ?>"
-				data-container="<?= $containerId ?>"
-				<?= $isAjax ? 'data-ajax="Y"' : '' ?>>
-			<svg width="18" height="18" viewBox="0 0 24 24" fill="none" class="load-more-icon">
-				<path d="M12 4V20M4 12H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+<!-- Классическая пагинация -->
+<ul class="pagination">
+<?
+if($arResult["bDescPageNumbering"] === true):
+	if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] + 1) ?>">
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
 			</svg>
-			Показать еще
-			<span class="load-more-count">(<?= $arResult["NavRecordCount"] - $arResult["NavLastRecordShow"] ?>)</span>
-		</button>
-
-		<!-- Разделитель -->
-		<span class="pagination-divider">или</span>
-	<? endif; ?>
-
-	<!-- Классическая пагинация -->
-	<ul class="pagination">
-	<?
-	if($arResult["bDescPageNumbering"] === true):
-		if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] + 1) ?>">
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</a>
-		</li>
-	<?
-		else:
-	?>
-		<li class="pagination-item disabled">
-			<span>
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</span>
-		</li>
-	<?
-		endif;
+		</a>
+	</li>
+<?
 	else:
-		if ($arResult["NavPageNomer"] > 1):
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] - 1) ?>">
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</a>
-		</li>
-	<?
-		else:
-	?>
-		<li class="pagination-item disabled">
-			<span>
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</span>
-		</li>
-	<?
+?>
+	<li class="pagination-item disabled">
+		<span>
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</span>
+	</li>
+<?
+	endif;
+else:
+	if ($arResult["NavPageNomer"] > 1):
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] - 1) ?>">
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</a>
+	</li>
+<?
+	else:
+?>
+	<li class="pagination-item disabled">
+		<span>
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</span>
+	</li>
+<?
+	endif;
+endif;
+
+
+if($arResult["bDescPageNumbering"] === true):
+	$bFirst = true;
+	if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+		if ($arResult["nStartPage"] < $arResult["NavPageCount"]):
+			$bFirst = false;
+			if($arResult["bSavePage"]):
+?>
+	<li class="pagination-item <?= ($arResult["NavPageCount"] == $arResult["NavPageNomer"]) ? 'active' : '' ?>">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["NavPageCount"] ?>">1</a>
+	</li>
+<?
+			else:
+?>
+	<li class="pagination-item <?= ($arResult["NavPageCount"] == $arResult["NavPageNomer"]) ? 'active' : '' ?>">
+		<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>">1</a>
+	</li>
+<?
+			endif;
+			if ($arResult["nStartPage"] < ($arResult["NavPageCount"] - 1)):
+?>
+	<li class="pagination-item disabled">
+		<span>...</span>
+	</li>
+<?
+			endif;
 		endif;
 	endif;
-
 	
-	if($arResult["bDescPageNumbering"] === true):
-		$bFirst = true;
-		if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
-			if ($arResult["nStartPage"] < $arResult["NavPageCount"]):
-				$bFirst = false;
-				if($arResult["bSavePage"]):
-	?>
-		<li class="pagination-item <?= ($arResult["NavPageCount"] == $arResult["NavPageNomer"]) ? 'active' : '' ?>">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["NavPageCount"] ?>">1</a>
-		</li>
-	<?
-				else:
-	?>
-		<li class="pagination-item <?= ($arResult["NavPageCount"] == $arResult["NavPageNomer"]) ? 'active' : '' ?>">
-			<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>">1</a>
-		</li>
-	<?
-				endif;
-				if ($arResult["nStartPage"] < ($arResult["NavPageCount"] - 1)):
-	?>
-		<li class="pagination-item disabled">
-			<span>...</span>
-		</li>
-	<?
-				endif;
-			endif;
+	do
+	{
+		$NavRecordGroupPrint = $arResult["NavPageCount"] - $arResult["nStartPage"] + 1;
+		if ($arResult["nStartPage"] == $arResult["NavPageNomer"]):
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>active">
+		<span><?= $NavRecordGroupPrint ?></span>
+	</li>
+<?
+		elseif($arResult["nStartPage"] == $arResult["NavPageCount"] && $arResult["bSavePage"] == false):
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
+		<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"><?= $NavRecordGroupPrint ?></a>
+	</li>
+<?
+		else:
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"><?= $NavRecordGroupPrint ?></a>
+	</li>
+<?
 		endif;
 		
-		do
-		{
-			$NavRecordGroupPrint = $arResult["NavPageCount"] - $arResult["nStartPage"] + 1;
-			if ($arResult["nStartPage"] == $arResult["NavPageNomer"]):
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>active">
-			<span><?= $NavRecordGroupPrint ?></span>
-		</li>
-	<?
-			elseif($arResult["nStartPage"] == $arResult["NavPageCount"] && $arResult["bSavePage"] == false):
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
-			<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"><?= $NavRecordGroupPrint ?></a>
-		</li>
-	<?
-			else:
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"><?= $NavRecordGroupPrint ?></a>
-		</li>
-	<?
-			endif;
-			
-			$arResult["nStartPage"]--;
-			$bFirst = false;
-		} while($arResult["nStartPage"] >= $arResult["nEndPage"]);
+		$arResult["nStartPage"]--;
+		$bFirst = false;
+	} while($arResult["nStartPage"] >= $arResult["nEndPage"]);
 
-		if ($arResult["NavPageNomer"] > 1):
-			if ($arResult["nEndPage"] > 1):
-				if ($arResult["nEndPage"] > 2):
-	?>
-		<li class="pagination-item disabled">
-			<span>...</span>
-		</li>
-	<?
-				endif;
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=1"><?= $arResult["NavPageCount"] ?></a>
-		</li>
-	<?
+	if ($arResult["NavPageNomer"] > 1):
+		if ($arResult["nEndPage"] > 1):
+			if ($arResult["nEndPage"] > 2):
+?>
+	<li class="pagination-item disabled">
+		<span>...</span>
+	</li>
+<?
 			endif;
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=1"><?= $arResult["NavPageCount"] ?></a>
+	</li>
+<?
 		endif;
-		
-	else:
-		$bFirst = true;
-		if ($arResult["NavPageNomer"] > 1):
-			if ($arResult["nStartPage"] > 1):
-				$bFirst = false;
-				if($arResult["bSavePage"]):
-	?>
-		<li class="pagination-item <?= ($arResult["NavPageNomer"] == 1) ? 'active' : '' ?>">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=1">1</a>
-		</li>
-	<?
-				else:
-	?>
-		<li class="pagination-item <?= ($arResult["NavPageNomer"] == 1) ? 'active' : '' ?>">
-			<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>">1</a>
-		</li>
-	<?
-				endif;
-				if ($arResult["nStartPage"] > 2):
-	?>
-		<li class="pagination-item disabled">
-			<span>...</span>
-		</li>
-	<?
-				endif;
-			endif;
-		endif;
-
-		do
-		{
-			if ($arResult["nStartPage"] == $arResult["NavPageNomer"]):
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>active">
-			<span><?= $arResult["nStartPage"] ?></span>
-		</li>
-	<?
-			elseif($arResult["nStartPage"] == 1 && $arResult["bSavePage"] == false):
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
-			<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"><?= $arResult["nStartPage"] ?></a>
-		</li>
-	<?
-			else:
-	?>
-		<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"><?= $arResult["nStartPage"] ?></a>
-		</li>
-	<?
-			endif;
-			
-			$arResult["nStartPage"]++;
+	endif;
+	
+else:
+	$bFirst = true;
+	if ($arResult["NavPageNomer"] > 1):
+		if ($arResult["nStartPage"] > 1):
 			$bFirst = false;
-		} while($arResult["nStartPage"] <= $arResult["nEndPage"]);
-
-		if($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
-			if ($arResult["nEndPage"] < $arResult["NavPageCount"]):
-				if ($arResult["nEndPage"] < ($arResult["NavPageCount"] - 1)):
-	?>
-		<li class="pagination-item disabled">
-			<span>...</span>
-		</li>
-	<?
-				endif;
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["NavPageCount"] ?>"><?= $arResult["NavPageCount"] ?></a>
-		</li>
-	<?
+			if($arResult["bSavePage"]):
+?>
+	<li class="pagination-item <?= ($arResult["NavPageNomer"] == 1) ? 'active' : '' ?>">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=1">1</a>
+	</li>
+<?
+			else:
+?>
+	<li class="pagination-item <?= ($arResult["NavPageNomer"] == 1) ? 'active' : '' ?>">
+		<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>">1</a>
+	</li>
+<?
+			endif;
+			if ($arResult["nStartPage"] > 2):
+?>
+	<li class="pagination-item disabled">
+		<span>...</span>
+	</li>
+<?
 			endif;
 		endif;
 	endif;
 
-	// ===== СТРЕЛКА ВПЕРЕД =====
-	if($arResult["bDescPageNumbering"] === true):
-		if ($arResult["NavPageNomer"] > 1):
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] - 1) ?>">
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</a>
-		</li>
-	<?
+	do
+	{
+		if ($arResult["nStartPage"] == $arResult["NavPageNomer"]):
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>active">
+		<span><?= $arResult["nStartPage"] ?></span>
+	</li>
+<?
+		elseif($arResult["nStartPage"] == 1 && $arResult["bSavePage"] == false):
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
+		<a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"><?= $arResult["nStartPage"] ?></a>
+	</li>
+<?
 		else:
-	?>
-		<li class="pagination-item disabled">
-			<span>
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</span>
-		</li>
-	<?
+?>
+	<li class="pagination-item <?= ($bFirst ? 'first ' : '') ?>">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"><?= $arResult["nStartPage"] ?></a>
+	</li>
+<?
 		endif;
-	else:
-		if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
-	?>
-		<li class="pagination-item">
-			<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] + 1) ?>">
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</a>
-		</li>
-	<?
-		else:
-	?>
-		<li class="pagination-item disabled">
-			<span>
-				<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-					<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-				</svg>
-			</span>
-		</li>
-	<?
+		
+		$arResult["nStartPage"]++;
+		$bFirst = false;
+	} while($arResult["nStartPage"] <= $arResult["nEndPage"]);
+
+	if($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+		if ($arResult["nEndPage"] < $arResult["NavPageCount"]):
+			if ($arResult["nEndPage"] < ($arResult["NavPageCount"] - 1)):
+?>
+	<li class="pagination-item disabled">
+		<span>...</span>
+	</li>
+<?
+			endif;
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["NavPageCount"] ?>"><?= $arResult["NavPageCount"] ?></a>
+	</li>
+<?
 		endif;
 	endif;
-	?>
-	</ul>
-</div>
+endif;
+
+// ===== СТРЕЛКА ВПЕРЕД =====
+if($arResult["bDescPageNumbering"] === true):
+	if ($arResult["NavPageNomer"] > 1):
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] - 1) ?>">
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</a>
+	</li>
+<?
+	else:
+?>
+	<li class="pagination-item disabled">
+		<span>
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</span>
+	</li>
+<?
+	endif;
+else:
+	if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+?>
+	<li class="pagination-item">
+		<a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] + 1) ?>">
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</a>
+	</li>
+<?
+	else:
+?>
+	<li class="pagination-item disabled">
+		<span>
+			<svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+				<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+			</svg>
+		</span>
+	</li>
+<?
+	endif;
+endif;
+?>
+</ul>
